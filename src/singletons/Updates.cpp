0c14b2d24aece825aba39a336d743a731e87a2f6
James Upjohn <jupjohn@jammeh.co.nz>
2026-01-19 16:33:01 +1300

    feat(updater): ignore major version when comparing 7.x.x to 2.y.y

--- src/singletons/Updates.cpp
+++ src/singletons/Updates.cpp
@@ -83,6 +83,14 @@ bool Updates::isDowngradeOf(const QString &online, const QString &current)
         return false;
     }
 
+    // TODO: remove once chatterino7's major version switches from `7` to `2`
+    if (currentVersion.major == 7 && onlineVersion.major == 2)
+    {
+        currentVersion = {2, onlineVersion.minor, onlineVersion.patch,
+                          onlineVersion.prerelease_type,
+                          onlineVersion.prerelease_number};
+    }
+
     return onlineVersion < currentVersion;
 }
 
