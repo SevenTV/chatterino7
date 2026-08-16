db630da3454725aabf0fbe75e1952fd10be3d041
Nerixyz <nerixdev@outlook.de>
2026-08-16 17:31:03 +0200

    feat: retry bttv & ffz on 429

---
+++ src/common/network/RetryingGet.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <functional>
+
+namespace chatterino {
+
+class NetworkResult;
+
+namespace network {
+
+/// Fetch `url` with backoff when 429 (Too Many Requests) is encountered.
+void fetchWithBackoff(const QUrl &url,
+                      std::function<void(NetworkResult)> onSuccess,
+                      std::function<void(NetworkResult)> onError,
+                      int timeout = 0);
+
+}  // namespace network
+
+}  // namespace chatterino
